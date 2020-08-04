0f0639ac3189c11ceee54672dda093e928edf2a8
kennycha <dudqn136@naver.com>
2020-08-04 13:26:25 +0900

    Add redirect without login

--- frontend/web-company/src/containers/surveys/DashboardViewerContainer.js
+++ frontend/web-company/src/containers/surveys/DashboardViewerContainer.js
@@ -2,28 +2,41 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import DashboardViewer from '../../components/surveys/DashboardViewer';
 import { readSurveysAnswers } from '../../modules/surveysAnswers';
+import { withRouter } from 'react-router-dom';
 
-const DashboardViewerContainer = () => {
+const DashboardViewerContainer = ({ history }) => {
   const dispatch = useDispatch();
-  const { surveysAnswers, error, loading, companyId } = useSelector(
+  const { surveysAnswers, error, loading, user, companyId } = useSelector(
     ({ surveysAnswers, loading, user }) => ({
       surveysAnswers: surveysAnswers.surveysAnswers,
       error: surveysAnswers.error,
       loading: loading['surveysAnswers/READ_SURVEYS_ANSWERS'],
       user: user.user,
-      companyId: user.user.companyId,
     }),
   );
   useEffect(() => {
+    if (!user) {
+      history.push('/login');
+    }
+  }, [history, user]);
+  if (user) {
+    const { companyId } = user;
+  }
+  useEffect(() => {
     dispatch(readSurveysAnswers({ companyId }));
   }, [dispatch, companyId]);
-  return (
-    <DashboardViewer
-      surveysAnswers={surveysAnswers}
-      error={error}
-      loading={loading}
-    />
-  );
+
+  if (user) {
+    return (
+      <DashboardViewer
+        surveysAnswers={surveysAnswers}
+        error={error}
+        loading={loading}
+      />
+    );
+  } else {
+    return <h1>hi</h1>;
+  }
 };
 
-export default DashboardViewerContainer;
+export default withRouter(DashboardViewerContainer);
