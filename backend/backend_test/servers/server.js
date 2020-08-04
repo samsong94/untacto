44ccd0e59c139b9f538af5b5a186a4eca0f9d8db
송재훈 <helheim@naver.com>
2020-08-04 15:47:48 +0900

    Update server.js

--- backend/backend_test/servers/server.js
+++ backend/backend_test/servers/server.js
@@ -4,6 +4,7 @@ const os = require('os');
 const app = express();
 const bodyParser = require('body-parser');
 const port =process.env.PORT || 3101;
+const cookieParser = require('cookie-parser');
 
 const route = require('../routes/index');
 const createSurveyRouter = require('../routes/beforeCreateSurvey');
@@ -15,13 +16,18 @@ const detailRouter = require('../routes/beforeSurveyDetail');
 const showSurveyListRouter = require('../routes/showSurveyList');
 
 app.use(bodyParser.json());
+app.use(cookieParser());
+
+//var companyId = express.request.cookies.companyId;
+
 app.use('/api/auth/signup', signUpRouter);
 app.use('/api/auth/login', loginRouter);
 app.use('/api/auth/logout', logoutRouter);
 app.use('/api/auth/check', checkRouter);
 app.use('/api/surveys',createSurveyRouter);
-app.use('/api/surveys/?',showSurveyListRouter);
-app.use('/api/surveys/${id}',detailRouter);
+app.use('/api/surveys?', showSurveyListRouter);
+app.use('/api/answers/',detailRouter);
+//app.use('/api/answers?',Router);
 //app.use('/api/surveys',express.static('uploads'));
 
 app.listen(port, ()=>{
