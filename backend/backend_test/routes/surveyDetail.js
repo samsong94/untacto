b7a010d881e2481b3c8cd055c8db2ffc43de30c5
root <root@ip-172-26-10-101.ap-northeast-2.compute.internal>
2020-08-05 13:13:23 +0900

    Update surveyDetail.js

--- backend/backend_test/routes/surveyDetail.js
+++ backend/backend_test/routes/surveyDetail.js
@@ -16,34 +16,62 @@ router.get('/',function(req,res,next){
 		database: 'project1'
 	});
 	connection.connect();
-	var sql = "select emotions from answer where surveyId="+surveyId+" and userId="+companyId+";";
-	connection.query(sql, function(err,rows,fields){
+	var sql1 = "select emotions from answer where surveyId="+surveyId+" and userId="+companyId+";";
+	var sql2 = "select title, kioskId, description_survey, createdAt, expiresAt from survey where surveyId = "+surveyId+" and userId = "+companyId+";";
+	var sql3 = "select userName from user where userId = "+companyId+";";
+	var sql4 = "select location from kiosk where kioskId = ";
+	//answer sql
+	var send_message="error";
+	connection.query(sql1,function(err,rows,fields){
 			if(!err){
-				console.log(rows);
 				console.log("answer select success");
-				var sql = "select title, kioskId, description_survey, createdAt, expiresAt from survey where surveyId = "+surveyId+" and userId = "+companyId+";";
-				connection.query(sql,function(err,rows,fields){
-						connection.end();
-						if(!err){
-							console.log(rows);
-							console.log("survey select success");
-							res.json({
-								title: rows[0]['title'],
-								kioskId: rows[0]['kioskId'],
-								description_survey: rows[0]["description_survey"],
-								createdAt: rows[0]['createdAt'],
-								expiresAt: rows[0]['expiresAt']
-							});
-						}
-						else{
-							console.log("survey select error");
-							console.log(err);
-						}
-				});
-				
+				send_message="answer:"+rows[0]['emotions']+",";
 			}
 			else{
 				console.log("answer select error");
+				console.log(err);
+			}
+	});
+	//survey sql
+	connection.query(sql2,function(err,rows,fields){
+			if(!err){
+				console.log("survey select success");
+				var title = rows[0]['title'];
+				var kioskId = rows[0]['kioskId'];
+				var description_survey = rows[0]['description_survey'];
+				var createdAt = rows[0]['createdAt'];
+				var expiresAt = rows[0]['expiresAt'];
+				sql4+=kioskId+";";
+				//kiosk sql
+				connection.query(sql4,function(err2,rows2,fields2){
+					if(!err2){
+						console.log("kiosk select success");
+						var location_kiosk = rows2[0]['location'];
+						send_message+="kiosk:{kioskId: "+kioskId+",location: "+location_kiosk+"}";
+						console.log(send_message);
+						res.json(send_message);
+					}
+					else{
+						console.log("kiosk select error");
+						console.log(err2);
+					}
+				});
+				send_message+="title:"+title+",createdAt:"+createdAt+",expiresAt:"+expiresAt+",description:"+description_survey+",";
+			}
+			else{
+				console.log("survey select error");
+				console.log(err);
+			}
+	});
+	//user sql
+	connection.query(sql3,function(err,rows,fields){
+			connection.end();
+			if(!err){
+				var companyName = rows[0]['userName'];
+				send_message+="user:{companyName:"+companyName+",userId:"+companyId+"},";
+			}
+			else{
+				console.log("user select error");
 				console.log(err);
 			}
 	});
