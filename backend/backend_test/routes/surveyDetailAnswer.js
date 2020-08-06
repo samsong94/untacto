a778acb146144d7ef26e6b4f49c7f1b5d78bd632
root <root@ip-172-26-10-101.ap-northeast-2.compute.internal>
2020-08-07 00:11:54 +0900

    Update surveyDetailAnswer.js

--- backend/backend_test/routes/surveyDetailAnswer.js
+++ backend/backend_test/routes/surveyDetailAnswer.js
@@ -16,8 +16,12 @@ router.get('/',function(req,res,next){
 	connection.connect();
 	var sql = "select emotions,timeIndex,customerId from answer where userId = "+ userId + " and surveyId = "+ surveyId +" order by timeIndex asc;";
 	var sql1 = "select COUNT(*) as cnt from answer where userId = " + userId+" and surveyId= " + surveyId+";";
-	var sql2 = "select MAX(customerId) as max from answer where userId = "+ userId+" and surveyId= " + surveyId+";";
+	var sql2 = "select COUNT(distinct customerId) as max from answer where userId = "+ userId+" and surveyId= " + surveyId+";";
 	var sql3 = "select MAX(timeIndex) as max from user where userId= "+userId+" and surveyId= " +surveyId+";";
+	var sql4 = "select distinct customerId from answer where userId= "+userId+" and surveyId= "+surveyId+";";
+	var sql5 = "select customerId from customer where gender = 'male' and (";
+	var sql6 = "select customerId from customer where gender = 'female' and (";
+
 	var anger = new Array();
 	var contempt = new Array();
 	var disgust = new Array();
@@ -26,24 +30,52 @@ router.get('/',function(req,res,next){
 	var neutral = new Array();
 	var sadness = new Array();
 	var surprise = new Array();
+	var customers = new Array();
+	var customer_female = new Array();
+	var customer_male = new Array();
+	var customer_young = new Array();
+	var customer_old = new Array();
 	var cnt;
 	var customer_cnt;
 	var max_timer;
+	var all, male, female, young, old;
 	connection.query(sql1,function(err,rows,fields){
 			if(!err){
 				cnt = rows[0]['cnt'];
-				console.log("cnt: "+cnt);
 			}
 	});
 	connection.query(sql2,function(err,rows,fields){
 			if(!err){
 				customer_cnt=rows[0]['max'];
-				console.log("customer_cnt: "+customer_cnt);
 			}
 	});
 	connection.query(sql3,function(err,rows,fields){
 			if(!err){
 				max_timer=rows[0]['max'];
+			}
+	});
+	connection.query(sql4,function(err,rows,fields){
+			if(!err){
+				for(var i =0; i<customer_cnt; i++){
+					customers.push(rows[i]['customerId']);
+					sql5+=" customerId = "+rows[i]['customerId'];
+					sql6+=" customerId = "+rows[i]['customerId'];
+					if(i!=customer_cnt-1){
+						sql5+=" or";
+						sql6+=" or";
+					}
+				}
+				sql5+=");";
+				sql6+=");";
+
+				//male chart
+				connection.query(sql5,function(err2,rows2,fields2){
+						if(!err){
+							
+						}
+						else{
+						}
+				}
 			}
 	});
 	connection.query(sql,function(err,rows,fields){
@@ -54,7 +86,7 @@ router.get('/',function(req,res,next){
 					var emotion=JSON.parse(rows[i]['emotions']);
 					emotions.push(emotion);
 				}
-				//All user's emotion sum during 15sec.
+				//All user's emotion sum during 15sec. This is for firstChart
 				for(var i =0; i<cnt; i++){
 					var sum_anger = 0;
 					var sum_contempt = 0;
@@ -64,8 +96,6 @@ router.get('/',function(req,res,next){
 					var sum_neutral = 0;
 					var sum_sadness = 0;
 					var sum_surprise =0;
-					console.log(i);
-					console.log("check");
 					for(var j=0; j<customer_cnt; j++){
 						sum_anger+=emotions[i].anger;
 						sum_contempt+=emotions[i].contempt;
@@ -88,7 +118,7 @@ router.get('/',function(req,res,next){
 					surprise.push(sum_surprise/customer_cnt);
 					console.log(anger);
 				}
-				res.json({
+				all={
 					anger:anger,
 					contempt: contempt,
 					disgust: disgust,
@@ -97,7 +127,7 @@ router.get('/',function(req,res,next){
 					neutral: neutral,
 					sadness: sadness,
 					surprise: surprise
-				});
+				};
 			}
 			else{
 				console.log("select emotion error");
