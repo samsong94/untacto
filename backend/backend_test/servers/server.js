170600efb9e6e108583a06235fa6d34454e04b4c
root <root@ip-172-26-10-101.ap-northeast-2.compute.internal>
2020-08-10 15:29:16 +0900

    Add customerLogin.js, Update server.js

--- backend/backend_test/servers/server.js
+++ backend/backend_test/servers/server.js
@@ -15,6 +15,7 @@ const checkRouter = require('../routes/check');
 const detailRouter = require('../routes/beforeSurveyDetail');
 const showSurveyListRouter = require('../routes/beforeShowSurveyList');
 const surveyDetailAnswerRouter = require('../routes/beforeSurveyDetailAnswer');
+const customerLoginRouter = require('../routes/customerLogin');
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -25,6 +26,7 @@ app.use('/api/auth/login', loginRouter);
 app.use('/api/auth/logout', logoutRouter);
 app.use('/api/auth/check', checkRouter);
 app.use('/api/surveys',createSurveyRouter);
+app.use('/api/customer/login',customerLoginRouter);
 app.use('/api/surveys?',function(req,res,next){res.locals.query = req.query; next(); }, showSurveyListRouter);
 app.use('/api/surveys/:id',function(req,res,next){res.locals.id=req.params.id; next();},detailRouter);
 app.use('/api/answers/:id', function(req,res,next){res.locals.id=req.params.id; next();},surveyDetailAnswerRouter);
