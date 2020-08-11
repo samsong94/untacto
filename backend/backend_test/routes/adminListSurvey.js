c8694099edb73fe0b50aa5fb78b81b880519fc97
root <root@ip-172-26-10-101.ap-northeast-2.compute.internal>
2020-08-11 17:43:44 +0900

    Update adminListSurvey.js

--- backend/backend_test/routes/adminListSurvey.js
+++ backend/backend_test/routes/adminListSurvey.js
@@ -21,148 +21,100 @@ router.get('/', function(req, res, next){
 		});
 		connection.connect();
 		var sql = "select * from survey";
+		var sql_user = "select * from user where userId = ";
+		var sql_kiosk = "select * from kiosk where kioskId = ";
 		var survey_list = new Array();
 		var page_count = page*10;
 		var next_count = (page+1)*10;
 		if(userId!=undefined&&kioskId!=undefined){
 			sql+=" where userId = "+userId+" and kioskId = "+kioskId+";";
-			connection.query(sql,function(err,rows,fields){
-				if(!err){
-					console.log("success both");
-					var i =0;
-					while(rows[i]!=undefined){
-						if(i>=page_count&&i<next_count){
-							var surveyId = rows[i]['surveyId'];
-							var userId = rows[i]['userId'];
-							var kioskId = rows[i]['kioskId'];
-							var title = rows[i]['title'];
-							var createdAt = rows[i]['createdAt'];
-							var expiresAt = rows[i]['expiresAt'];
-							var beginsAt = rows[i]['beginsAt'];
-							var survey = {
-								surveyId:surveyId,
-								userId: userId,
-								kioskId: kioskId,
-								title: title,
-								createdAt: createdAt,
-								expiresAt: expiresAt,
-								beginsAt: beginsAt
-							};
-							survey_list.push(survey);
-						}
-						i++;
-					}
-					res.json(survey_list);
-				}
-				else{
-					console.log(err);
-				}
-			});
 		}
 		else if(userId!=undefined){
 			sql+=" where userId = "+userId+";";
-			connection.query(sql,function(err,rows,fields){
-				if(!err){
-					console.log("success userId");
-					var i =0;
-					while(rows[i]!=undefined){
-						if(i<=page_count&&i<next_count){
-							var surveyId = rows[i]['surveyId'];
-							var userId = rows[i]['userId'];
-							var kioskId = rows[i]['kioskId'];
-							var title = rows[i]['title'];
-							var createdAt = rows[i]['createdAt'];
-							var expiresAt = rows[i]['expiresAt'];
-							var beginsAt = rows[i]['beginsAt'];
-							var survey = {
-								surveyId:surveyId,
-								userId: userId,
-								kioskId: kioskId,
-								title: title,
-								createdAt: createdAt,
-								expiresAt: expiresAt,
-								beginsAt: beginsAt
-							};
-							survey_list.push(survey);
-						}
-						i++;
-					}
-					res.json(survey_list);
-				}
-				else{
-					console.log(err);
-				}
-			});
 		}
 		else if(kioskId!=undefined){
 			sql+=" where kioskId = "+kioskId+";";
-			connection.query(sql,function(err,rows,fields){
-					if(!err){
-						console.log("success kioskId");
-						var i =0;
-						while(rows[i]!=undefined){
-							if(i>=page_count&&i<next_count){
-								var surveyId = rows[i]['surveyId'];
-								var userId = rows[i]['userId'];
-								var kioskId = rows[i]['kioskId'];
-								var title = rows[i]['title'];
-								var createdAt = rows[i]['createdAt'];
-								var expiresAt = rows[i]['expiresAt'];
-								var beginsAt = rows[i]['beginsAt'];
+		}
+		var surveyId=new Array();
+		userId = new Array();
+		kioskId = new Array();
+		var title = new Array();
+		var createdAt = new Array();
+		var expiresAt = new Array();
+		var beginsAt = new Array();
+		connection.query(sql,function(err,rows,fields){
+			if(!err){
+				var i =0;
+				while(rows[i]!=undefined){
+					if(i>=page_count&&i<next_count){
+						surveyId.push(rows[i]['surveyId']);
+						userId.push(rows[i]['userId']);
+						kioskId.push(rows[i]['kioskId']);
+						title.push(rows[i]['title']);
+						createdAt.push(rows[i]['createdAt']);
+						expiresAt.push(rows[i]['expiresAt']);
+						beginsAt.push(rows[i]['beginsAt']);
+					}
+					i++;
+				}
+				i = 0;
+				while(userId[i]!=undefined){
+					var user, kiosk;
+					sql_user_tmp=sql_user+userId[i];
+					sql_kiosk_tmp=sql_kiosk+kioskId[i];
+					connection.query(sql_user_tmp,function(err2,rows2,fields2){
+							if(!err2){
+								console.log("user select success");
+								var userName = rows2[0]['userName'];
+								var email = rows2[0]['email'];
+								var createdAt_user = rows2[i]['createdAt'];
+								user = {
+									userId: userId,
+									userName: userName,
+									email: email,
+									createdAt: createdAt_user
+								};
+							}
+							else{
+								console.log("user select error");
+								
+							}
+					});
+					connection.query(sql_kiosk_tmp,function(err2,rows2,fields2){
+							if(!err2){
+								console.log("kiosk select success");
+								var location = rows2[0]['location'];
+								kiosk = {
+									kioskId: kioskId,
+									location: location
+								};
 								var survey = {
 									surveyId:surveyId,
-									userId: userId,
-									kioskId: kioskId,
-									title: title,
-									createdAt: createdAt,
-									expiresAt: expiresAt,
-									beginsAt: beginsAt
+									user:user,
+									kiosk:kiosk,
+									title:title,
+									createdAt:createdAt,
+									expiresAt:expiresAt,
+									beginsAt:beginsAt
 								};
 								survey_list.push(survey);
+								console.log(survey_list);
+								if(userId[i+1]==undefined){
+									res.json(survey_list);
+								}
 							}
-							i++;
-						}
-						res.json(survey_list);
-					}
-					else{
-						console.log(err);
-					}
-			});
-		}
-		else{
-			connection.query(sql,function(err,rows,fields){
-					if(!err){
-						console.log("success zero");
-						var i =0;
-						while(rows[i]!=undefined){
-							if(i>=page_count&&i<next_count){
-								var surveyId = rows[i]['surveyId'];
-								var userId = rows[i]['userId'];
-								var kioskId = rows[i]['kioskId'];
-								var title = rows[i]['title'];
-								var createdAt = rows[i]['createdAt'];
-								var expiresAt = rows[i]['expiresAt'];
-								var beginsAt = rows[i]['beginsAt'];
-								var survey = {
-									surveyId:surveyId,
-									userId: userId,
-									kioskId: kioskId,
-									title: title,
-									createdAt: createdAt,
-									expiresAt: expiresAt,
-									beginsAt: beginsAt
-								};
-								survey_list.push(survey);
+							else{
+								console.log("kiosk select error");
 							}
-							i++;
-						}
-						res.json(survey_list);
-					}
-					else{
-						console.log(err);
-					}
-			});
-		}
+					});
+					i++;
+				}
+			}
+			else{
+				console.log(err);
+			}
+		});
+		
 });
 
 module.exports = router;
