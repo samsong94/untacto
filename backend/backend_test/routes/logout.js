f95de34bd5c14094d2584d14ce47dcf41cf50d67
송재훈 <helheim@naver.com>
2020-08-19 17:01:43 +0900

    Update logout.js

--- backend/backend_test/routes/logout.js
+++ backend/backend_test/routes/logout.js
@@ -6,12 +6,11 @@ var cookieParser = require('cookie-parser')
 router.use(cookieParser());
 
 router.post('/', function (req, res, next) {
-	//console.log('logout: ' + req.cookies.user + '.');
 	res.clearCookie('user');
 	res.clearCookie('userName');
 	res.clearCookie('userId');
 	res.clearCookie('tok');
-	res.redirect('/');
+	res.redirect('/api/auth/check');
 });
 
 module.exports = router;
