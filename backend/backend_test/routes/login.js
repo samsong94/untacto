0e6d7e96ade62d37b7a9866e032f231e28aaf3c0
송재훈 <helheim@naver.com>
2020-08-20 15:34:46 +0900

    Update login.js

--- backend/backend_test/routes/login.js
+++ backend/backend_test/routes/login.js
@@ -3,10 +3,11 @@ var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
 var mysql = require('mysql');
-
+var bcrypt = require('bcrypt');
 const secret="ThISisSecRETKeY";
 
 router.post('/', function (req, res, next) {
+	var saltRounds = 10;
     var email = req.body['email'];
     var password = req.body['password'];
     var connection = mysql.createConnection({
@@ -18,37 +19,40 @@ router.post('/', function (req, res, next) {
 	});
     connection.connect();
 
-    connection.query('select * from user where email=\'' + email + '\' and password=\'' + password + '\'', function (err, rows, fields) {
+    connection.query('select * from user where email=\'' + email + '\';', function (err, rows, fields) {
         if (!err) {
-            if (rows[0]!=undefined) {
-				const token = jwt.sign({
-					id:rows[0]['userId'],
-					exp:Math.floor(Date.now()/1000) + (60*60)
-				},
-				secret);
-				var user = {
-					companyName: rows[0]['userName'],
-					companyId: rows[0]['userId']
-				};
-				
-				res.cookie('user', user);
-				res.cookie('tok',token);
-				res.cookie('companyName', rows[0]['userName']);
-				res.cookie('companyId', rows[0]['userId']);
-				res.json({
-					result: 'ok',
-					token
-				});
-			} else {
-				res.status(403).json({
-					message: err
-				})
-            }
-
+			bcrypt.compare(password, rows[0]['password'], function(err_hash, result){ 
+				if(result) {
+		            if (rows[0]!=undefined) {
+						const token = jwt.sign({
+							id:rows[0]['userId'],
+							exp:Math.floor(Date.now()/1000) + (60*60)
+						},
+						secret);
+						var user = {
+							companyName: rows[0]['userName'],
+							companyId: rows[0]['userId']
+						};
+						
+						res.cookie('user', user);
+						res.cookie('tok',token);
+						res.cookie('companyName', rows[0]['userName']);
+						res.cookie('companyId', rows[0]['userId']);
+						res.json({
+							result: 'ok',
+							token
+						});
+					}
+				} else {
+					res.status(403).json({
+						message: 'your password is incorrect'
+					});
+				}
+			});
         } else {
 			res.status(403).json({
 				message: err
-			})
+			});
         }
     });
 });
