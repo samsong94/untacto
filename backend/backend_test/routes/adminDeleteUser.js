1cdbccf7f2e982ff43c5301c60b10ee2af14beae
송재훈 <helheim@naver.com>
2020-08-20 15:34:02 +0900

    Update adminDeleteUser.js

--- backend/backend_test/routes/adminDeleteUser.js
+++ backend/backend_test/routes/adminDeleteUser.js
@@ -22,11 +22,21 @@ router.delete('/', function(req, res, next){
 	connection.connect();
 	
 	//sql query
-	var sql_survey = 'delete from user where userId=' + id + ';';
-	connection.query(sql_survey,function(err){
+	var sql_user = 'delete from user where userId=' + id + ';';
+	var sql_survey = 'delete from survey where userId=' + id + ';';
+	connection.query(sql_user, function(err){
 		if(!err){
 			console.log("delete user success");
-			res.json({result:"ok"});
+			connection.query(sql_survey, function(err2){
+				if(!err2){
+					console.log("delete surveys success");
+					res.json({result:"ok"});
+				}
+				else{
+					console.log("delete surveys error");
+					res.json({error:err2});
+				}
+			});
 		}
 		else{
 			console.log("delete delete error");
